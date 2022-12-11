4ceef82877d1861425720d43284fa87996d5bd1e
Kris Andersen <kris@bitmandu.com>
2022-12-10 22:57:26 -0500

    Fake gpio_set_level() and gpio_get_level()

--- driver/gpio/stub_gpio.c
+++ driver/gpio/stub_gpio.c
@@ -6,22 +6,40 @@
 
 #include "stub_gpio.h"
 
-static gpio_config_t pin_config[64];
+/**< GPIO levels */
+typedef enum { UNKNOWN = -1, LOW, HIGH } level_t;
+
+static struct {
+    gpio_config_t config;
+    level_t level;
+} gpio_state[GPIO_NUM_MAX];
 
 esp_err_t gpio_config(const gpio_config_t *pGPIOConfig)
 {
-    for (gpio_num_t i = 0; i < 64; ++i) {
+    for (gpio_num_t i = 0; i < GPIO_NUM_MAX; ++i) {
         if (pin_is_set(pGPIOConfig->pin_bit_mask, i)) {
-            pin_config[i] = *pGPIOConfig;
+            gpio_state[i].config = *pGPIOConfig;
+            gpio_state[i].level = UNKNOWN;
         }
     }
 
     return ESP_OK;
 }
 
+esp_err_t gpio_set_level(gpio_num_t gpio_num, uint32_t level)
+{
+    gpio_state[gpio_num].level = level ? HIGH : LOW;
+    return ESP_OK;
+}
+
+int gpio_get_level(gpio_num_t gpio_num)
+{
+    return gpio_state[gpio_num].level;
+}
+
 gpio_config_t *gpio_config_spy(gpio_num_t pin)
 {
-    return &pin_config[pin];
+    return &gpio_state[pin].config;
 }
 
 bool pin_is_set(uint64_t pin_bit_mask, gpio_num_t pin)
